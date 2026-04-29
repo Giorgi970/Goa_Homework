945f336bb6efb947359edae06e72ef91f8ce8001
Giorgi970 <giorgi.kukhianidze2011@gmail.com>
2026-04-29 18:45:54 +0400

    commit

---
+++ level 0/ClassWork/cw1.py
@@ -0,0 +1,38 @@
+print("Giorgi")
+print("Laptop")
+print("School")
+print("Python")
+print("hello")
+
+print(5)
+print(100)
+print(20)
+print(15)
+print(13)
+
+print(3.4)
+print(2.4)
+print(2.5)
+print(2.6)
+print(2.7)
+
+print(5 + 5)
+print(2 + 2)
+print(3 + 3)
+
+print(2 - 1)
+print(3 - 1)
+print(4 - 2)
+ 
+print(3 * 8)
+print(2 * 8)
+print(1 * 8)
+
+print(10 / 1)
+print(2/1)
+print(3/1)
+
+string_aris textureis  monacemebi igive sityvebi da asoebi
+integer_ mteli ricxvi maglitad 2 da ara 2.5
+float_aris atwiladi ricxvi
+cvladi_aris adgili sadac vinaxavt monacemebs
--- level 1/classwork/cw1.py
+++ level 1/classwork/cw1.py
@@ -11,4 +11,5 @@ print(2*2)
 print(3*3)
 print(9/3)
 print(12/4)
+        
 
