6306713ce81069abdf16d7cf9d4efd094c9c218a
Giorgi970 <giorgi.kukhianidze2011@gmail.com>
2026-05-19 22:03:52 +0400

    commit

--- level 1/classwork/cw1.py
+++ level 1/classwork/cw1.py
@@ -1,6 +1,20 @@
-saxeli=Giorgi
-asaki=1
-feri=lurji
-manqana=bmw
-qalaqi=kutaisi
-print="Me mqvia"+ saxeli +"var"+asaki+"wlis"+"sayvareli feri"+feri+"sayvareli manqana"+manqana+"kalaki"+qalaqi
+print(5 > 3)
+print(8 > 4)
+print(9 > 3)
+print(8 > 4)
+print(5 > 2)
+
+print(6 > 9)
+print(9 > 11)
+print(2 > 4)
+print(8 > 9)
+print(1 > 3)      
+for i in(100):
+    print(kukhianidze)
+for i in(5):
+    print(bmw)
+for i in(46):
+    print(witeli)
+for i in(32):
+    print(g)
+    
