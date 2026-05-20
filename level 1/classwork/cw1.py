f4096d0f609700dcd510732d2d38b4041fd1d315
Giorgi970 <giorgi.kukhianidze2011@gmail.com>
2026-05-20 20:53:21 +0400

    commit

--- level 1/classwork/cw1.py
+++ level 1/classwork/cw1.py
@@ -1,20 +1,2 @@
-print(5 > 3)
-print(8 > 4)
-print(9 > 3)
-print(8 > 4)
-print(5 > 2)
 
-print(6 > 9)
-print(9 > 11)
-print(2 > 4)
-print(8 > 9)
-print(1 > 3)      
-for i in(100):
-    print(kukhianidze)
-for i in(5):
-    print(bmw)
-for i in(46):
-    print(witeli)
-for i in(32):
-    print(g)
     
