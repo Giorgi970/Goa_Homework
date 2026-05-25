c10b17b9ebf78014c1af05dcd006e5dfbe40371b
Giorgi970 <giorgi.kukhianidze2011@gmail.com>
2026-05-25 22:16:23 +0400

    commit

--- level 1/classwork/cw1.py
+++ level 1/classwork/cw1.py
@@ -1,14 +1,17 @@
-print("shaurma")
-print("fexburti")
-print("manqanebi")
-print("leptopi")
-print("kompiuteri")
-print("xili")
-print("bostneuli")
-print("seirnoba")
-print("tamashi")
-print("simgeris mosmena")
-print("kinos yureba")
-print("kitxva")
-print("vegar vipikreb ocamde")
-print("level 1 shesrulebulia")
+print("codi sedgeba 2 nawilisgan mag print 2 ki mnishvneloba da monacemi mag teqsti")
+print( 2 + 3)
+print( 3 - 2)
+print( 4 * 2)
+print( 4 / 2)
+
+print(10 / 5)
+print("chveulebrivi gayopa")
+print(10/4)
+print('mtel ricxvad gayopa')
+
+name="gio"
+age="14"
+city="kutaisi"
+height="175"
+hobby="coding"
+print(name, age, city, heigth, hobby,)
