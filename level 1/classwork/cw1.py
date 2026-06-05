dc94bee796aa9ce234224478fbfddd534e69c926
Giorgi970 <giorgi.kukhianidze2011@gmail.com>
2026-06-05 17:53:52 +0400

    commit

---
+++ level 1/classwork/cw1.py
@@ -0,0 +1,29 @@
+#1)ra aris for loop ?
+#for loop aris moqmedeb rodesac erti da igive ragacas bevrjer vimeorebt
+#2)კომენტარების სახით ახსენით თუ რაარის range() და რაგადეცემა მას
+#rage qmnis ricxvebis serias gadaecema starti stopi da stepi
+
+#4)დაბეჭდეთ რიცხვები 0 იდან 100 ამდე 2-2 ის გამოტოებით
+for i in range(0,101,2):
+    print(i)
+    
+
+#5) დაბეჭდეთ ტერმინალში თქვენი სახელის პირველი ასო 32 ჯერ
+for i in range(32):
+    print("g")
+#6)მომხმარებელს შემოატაინეთ რიცხვი და შეამოწმებთ თუ მისი ასაკი მეტია 18 ზე გამოტანეთ "სრულწლოვანი" და თუ ნაკლებია "ბავშვი"
+age=int(input("enter your age: "))
+if age>18:
+    print("srulwlovani")
+else:
+    print("bavshvi")
+#7)მომხმარებელს შემოატანინეთ მისი ასაკი 13 ჯერ გამოიყენე for loop
+for i in range(13):
+    input("enter number")
+#8)დაატრიალეთ for ციკლი 10 დან 100 მდე 3 step ის გამოტოვებით და გამოიტანეთ თითოეული რიცხვი ტერმინალში
+for i in range(10,100, 3):
+    print(i)
+    
+#9)კომენტარების სახით ახსნით როგორი სახის list-ები გვაქვს html-ში და ასევე გააკეტეთ თითო მაგალითი
+#dalagebuli da daulagebeli
+
